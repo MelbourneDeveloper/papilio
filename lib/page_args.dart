c2f004873422442159b80a32f40e05732618ff60
Christian Findlay <melbournedeveloper@users.noreply.github.com>
2022-07-10 19:00:40 +1000

    Formatting

--- lib/page_args.dart
+++ lib/page_args.dart
@@ -6,7 +6,7 @@ class PageArgs {
   final Object? pageScope;
   final Object? arguments;
 
-  ///No need to access this. We only keep a reference to this so we can 
+  ///No need to access this. We only keep a reference to this so we can
   ///dispose it on pop
   final Bloc bloc;
 
